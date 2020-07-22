7688ff9649a612e34c69638ef493f4ec758e62a0
fei-hsu <ge54sik@mytum.de>
2020-07-22 22:46:47 +0200

    Add comment, update dmap linear classify

--- models/nn_dmap.py
+++ models/nn_dmap.py
@@ -6,12 +6,12 @@ import torch.nn as nn
 from datafold.dynfold import DiffusionMaps
 import datafold.pcfold as pfold
 
-class CNN_DMap(nn.Module):
+class NN_DMap(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.fc = nn.Sequential(
-            nn.Linear(2, 512),
+            nn.Linear(3, 512),
             nn.ReLU(),
             nn.Dropout(0.2),
             nn.Linear(512, 10),
@@ -23,14 +23,14 @@ class CNN_DMap(nn.Module):
         X_pcm = pfold.PCManifold(x)
         X_pcm.optimize_parameters(result_scaling=2)
         dmap = DiffusionMaps(kernel=pfold.GaussianKernel(epsilon=X_pcm.kernel.epsilon), 
-                     n_eigenpairs=6, dist_kwargs=dict(cut_off=X_pcm.cut_off))
+                     n_eigenpairs=10, dist_kwargs=dict(cut_off=X_pcm.cut_off))
 
         dmap = dmap.fit(X_pcm)
-        dmap = dmap.set_coords([1, 2])
+        dmap = dmap.set_coords([1, 2, 3])
         X_pcm = dmap.transform(X_pcm)
         X_pcm = torch.from_numpy(X_pcm).float()
         #print(X_pcm.shape)
 
-        x = X_pcm.view(-1, 2)
+        x = X_pcm.view(-1, 3)
         x = self.fc(x)
         return x
